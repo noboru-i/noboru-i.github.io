4d72fab71536bb4a4e58b2e7ab674152e8aae4f3
noboru-i <ishikura.noboru@gmail.com>
2015-04-05 22:01:13 +0900

    circle.ymlの作成

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,6 +10,11 @@ cd public
 git init
 git remote add origin git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
 
+echo 'general:' > circle.yml
+echo '  branches:' >> circle.yml
+echo '    ignore:' >> circle.yml
+echo '      - master' >> circle.yml
+
 git add --force .
 git commit -m "Release $CIRCLE_BUILD_NUM"
 git push -f origin master:master
