988009a8614bf983a8f0f4f17c509b63aadcce0d
noboru-i <ishikura.noboru@gmail.com>
2015-04-05 21:18:48 +0900

    public以下をmasterにpushするように変更

---
+++ script/deploy.sh
@@ -0,0 +1,15 @@
+npm run build
+
+git config --global user.email ishikura.noboru@gmail.com
+git config --global user.name "CircleCI"
+
+mkdir $CIRCLE_ARTIFACTS/public
+cp public/* $CIRCLE_ARTIFACTS/public/
+
+cd public
+git init
+git remote add origin git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
+
+git add --force .
+git commit -m "Release $CIRCLE_BUILD_NUM"
+git push -f origin $CIRCLE_BRANCH:master
