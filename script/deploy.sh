ef4d8e4bf2a46d4e6d5beebb47adfe8f6934220e
noboru-i <ishikura.noboru@gmail.com>
2015-04-16 23:27:58 +0900

    circle.ymlをコピーするように変更

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,10 +9,7 @@ cd public
 git init
 git remote add origin git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
 
-echo 'general:' > circle.yml
-echo '  branches:' >> circle.yml
-echo '    ignore:' >> circle.yml
-echo '      - master' >> circle.yml
+cp ../circle.yml circle.yml
 
 git add --force .
 git commit -m "Release $CIRCLE_BUILD_NUM"
