cdd88c328bb4bdeb02bf716d16031917d898b9b1
noboru-i <ishikura.noboru@gmail.com>
2018-04-19 23:54:22 +0900

    add apple touch icons

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,7 +8,14 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by noboru-i" />
+        <Helmet defaultTitle="Blog by noboru-i">
+          <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/favicons/touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-startup-image" href="/favicons/launch.png" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        </Helmet>
         {children()}
       </div>
     );
